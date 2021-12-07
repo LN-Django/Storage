9a70cbd101ec850ac744a32933f0f24694245051
Louis Andrew <louisandrew3@gmail.com>
2021-12-07 12:21:11 +0100

    fix(post_storage_info): fix key error bug when returning response

--- boilerplate/storage_service/services.py
+++ boilerplate/storage_service/services.py
@@ -19,7 +19,6 @@ class StorageInfoService():
             delivery_time=storage_data['delivery_time'], product_id=storage_data['product_id'])
 
         response_data = storage_data
-        del response_data['id']
         return response_data
 
     def get_storage_information_by_product_id(product_id: int) -> Product:
